d33968b687f6106f403f86efd8e87a48548a096a
RaviTheRockStar <ravitherockstar1999@gmail.com>
2024-09-18 04:25:06 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -36,7 +36,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("https://github.com/RaviTheRockStar")
+    return web.json_response("https://github.com/RaviTheRockStar/text-leech-bot")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
